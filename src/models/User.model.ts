1166d0e64e63cae4098298c86293e8bef1e16068
Castor1983 <a0958086750@gmail.com>
2023-10-16 15:53:41 +0300

    start hw8

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,36 +1,46 @@
-import {model, Schema} from 'mongoose';
-import {EGenders} from '../enums/gender.enum';
+import { model, Schema } from "mongoose";
 
+import { EGenders } from "../enums/gender.enum";
+import { EUserStatus } from "../enums/user-status.enum";
+import { IUser } from "../types/user.type";
 
-const userSchema = new Schema({
-        name: {
-            type: String,
-        },
-        age: {
-            type: Number,
-            min: [1, 'minimum age is 1'],
-            max: [110, 'maximum age is 110']
-        },
-        genders: {
-            type: String,
-            enum: EGenders,
-        },
-        email: {
-            type: String,
-            required: true,
-            lowercase: true,
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    },);
+    age: {
+      type: Number,
+      min: [1, "Minimum age is 1"],
+      max: [199, "Maximum age is 199"],
+    },
+    genders: {
+      type: String,
+      enum: EGenders,
+    },
+    status: {
+      type: String,
+      enum: EUserStatus,
+      required: true,
+      default: EUserStatus.inactive,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      select: false,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
-export const User = model("user", userSchema);
+export const User = model<IUser>("user", userSchema);
